359f834030c854ddf6e3a5b21c1aef83d46b86ec
Alejandro Alfaro <alejandro.alfaro@intive.com>
2025-07-15 17:32:25 -0600

    Update Catalog API  - CQRS implementation - Introduced `ProductNotFoundException` for error handling. - Added endpoints for product deletion, retrieval by category, retrieval by ID, and updating a product using `ICarterModule`. - Implemented command and query classes following the CQRS pattern. - Created handlers for commands and queries with logging and database operations. - Defined response models to standardize API responses.

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct;
+
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductCommandResult>;
+
+public record DeleteProductCommandResult(bool IsSuccess);
+
+
+internal class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommand> logger) : ICommandHandler<DeleteProductCommand, DeleteProductCommandResult>
+{
+    public async Task<DeleteProductCommandResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Query}", command);
+
+        session.Delete<Product>(command.Id);
+        
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductCommandResult(true);
+    }
+}
