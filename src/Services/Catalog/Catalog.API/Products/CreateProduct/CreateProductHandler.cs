654e443561f75995ad469bc5aa51188fbe94b49c
Alejandro Alfaro <alejandro.alfaro@intive.com>
2025-07-16 15:48:28 -0600

    Add validation and mapping support with new packages Updated `BuildingBlocks.csproj` to include `FluentValidation`, `Mapster`, and `MediatR` for enhanced validation, mapping, and command handling. Modified `GlobalUsing.cs` to include global using directives for the new packages. Introduced `CreateProductCommandValidator` in `CreateProductHandler.cs` to enforce validation rules for product creation commands. Updated `CreateProductCommandHandler` to validate commands using the new validator and throw a `ValidationException` on validation failure. Enhanced service registration in `Program.cs` to automatically register validators from the assembly.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,12 +1,36 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using FluentValidation;
+
+namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price):ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required.");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Image file is required.");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than zero.");
+    }
+}
+
+internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+
+        var result = await validator.ValidateAsync(command, cancellationToken);
+        var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+
+        if (errors.Count > 0)
+        {
+            throw new ValidationException(result.Errors);
+        }
+
         var product = new Product()
         {
             Name = command.Name,
