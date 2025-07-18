c919c9a8bb51b7294bb06978e43244a7e05a4725
Alejandro Alfaro <alejandro.alfaro@intive.com>
2025-07-17 20:29:53 -0600

    Basket API - First version of the endpoints with mock responses. Add basket management endpoints and models Updated project configuration to include new packages and project references. Implemented endpoints for deleting, retrieving, and storing baskets using Carter and MediatR. Added command and query handlers for basket operations. Introduced models for ShoppingCart and ShoppingCartItem. Modified launch settings for local development.

---
+++ src/Services/Basket/BasketAPI/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+namespace BasketAPI.Basket.DeleteBasket;
+
+//public record DeleteBasketRequest(string UserName);
+
+public record DeleteBasketResponse(bool IsSuccess);
+
+public class DeleteBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{userName}", async (ISender sender, string userName) => 
+        { 
+            var command = await  sender.Send(new DeleteBasketCommand(userName));
+
+            var response = command.Adapt<DeleteBasketResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteProduct")
+        .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete Product")
+        .WithDescription("Delete Product");
+    }
+}
---
+++ src/Services/Basket/BasketAPI/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using BasketAPI.Models;
+global using BuildingBlocks.CQRS;
+global using Carter;
+global using FluentValidation;
+global using Mapster;
+global using MediatR;
+global using BuildingBlocks.Behaviors;
